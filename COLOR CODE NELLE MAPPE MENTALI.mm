f8e3f311f11e2f051c366c32cb8696b9aa2f0e9a
Stefano <stefano.g.magni@hotmail.it>
2024-01-12 12:33:38 +0100

    'Aggiunto COLOR CODE NELLE MAPPE MENTALI.mm'

---
+++ COLOR CODE NELLE MAPPE MENTALI.mm
@@ -0,0 +1,175 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="🎨&#xa;Creatività Visiva nelle Mappe Mentali" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1606664858024" MODIFIED="1704794803572" ICON_SIZE="16 pt" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="7.6 pt" SHAPE_VERTICAL_MARGIN="5 pt" UNIFORM_SHAPE="true" TEXT_ALIGN="CENTER" VGAP_QUANTITY="3 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="3.1 px">
+<font NAME="Inter Medium" SIZE="30" BOLD="true"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<hook NAME="MapStyle" background="#3c3836" zoom="0.77">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#0047abff,#50c878ff,#d56231ff,#9966ccff,#cb4154ff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" associatedTemplateLocation="template:/dark_gruvbox_template.mm" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_602083445" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#3c3836" BACKGROUND_COLOR="#fbf1c7" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#a89984" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_602083445" STARTINCLINATION="102.75 pt;-17.25 pt;" ENDINCLINATION="102.75 pt;3 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#93a1a1" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#ffffff" BACKGROUND_COLOR="#458588" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#458588"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_749235638" BORDER_WIDTH="3 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#cc241d">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#cc241d" TRANSPARENCY="255" DESTINATION="ID_749235638"/>
+<font SIZE="12" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#fdf6e3" BACKGROUND_COLOR="#282828" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_WIDTH="3.1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font NAME="Ubuntu" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#282828" BACKGROUND_COLOR="#fbf1c7" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font NAME="Ubuntu" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#282828" BACKGROUND_COLOR="#ebdbb2" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#282828" BACKGROUND_COLOR="#d79921" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#ffffff" BACKGROUND_COLOR="#98971a" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#ffffff" BACKGROUND_COLOR="#b16286" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" COLOR="#ffffff" BACKGROUND_COLOR="#689d6a" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" COLOR="#ffffff" BACKGROUND_COLOR="#a89984" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#ebdbb2" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0" BACKGROUND_COLOR="#ebdbb2">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook URI="../OneDrive/Desktop/231222%20-%20Copertina.svg" NAME="ExternalObject"/>
+<edge STYLE="sharp_bezier" WIDTH="14"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="24" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Introduzione" FOLDED="true" POSITION="top_or_left" ID="ID_1291783714" CREATED="1704643663264" MODIFIED="1704998131841" HGAP_QUANTITY="-481.74999 pt" VSHIFT_QUANTITY="178.49999 pt" VGAP_QUANTITY="3 pt" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" UNIFORM_SHAPE="true" FORMAT="markdownPatternFormat" TEXT_ALIGN="CENTER" BORDER_WIDTH="3.5 px">
+<font NAME="Inter Medium" SIZE="20"/>
+<edge STYLE="sharp_bezier" COLOR="#50c878" WIDTH="18"/>
+<node TEXT="Ravviva l&apos;apprendimento con il color coding" FOLDED="true" ID="ID_888886570" CREATED="1704643875493" MODIFIED="1704644828513" HGAP_QUANTITY="-550.74998 pt" VSHIFT_QUANTITY="-3.75 pt" VGAP_QUANTITY="3 pt" COLOR="#ffffff" BACKGROUND_COLOR="#632bf1" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH="3.5 px">
+<font NAME="Inter Medium"/>
+<edge STYLE="sharp_bezier" WIDTH="9"/>
+<node TEXT="Importanza" FOLDED="true" ID="ID_1825835824" CREATED="1704643216169" MODIFIED="1704644834414" HGAP_QUANTITY="-452.49999 pt" VSHIFT_QUANTITY="4.5 pt" VGAP_QUANTITY="3 pt" BORDER_WIDTH="3.5 px">
+<edge STYLE="sharp_bezier" WIDTH="9"/>
+<font NAME="Inter Medium"/>
+<node TEXT="Rinnova" FOLDED="true" ID="ID_1289461688" CREATED="1704643308264" MODIFIED="1704644809904" HGAP_QUANTITY="-204.24999 pt" VSHIFT_QUANTITY="-11.25 pt" VGAP_QUANTITY="3 pt" BORDER_WIDTH="3.5 px">
+<edge STYLE="sharp_bezier" WIDTH="9"/>
+<font NAME="Inter Medium"/>
+<node TEXT="Memorizzazione" ID="ID_677044284" CREATED="1704643311841" MODIFIED="1704644845777" HGAP_QUANTITY="-216.24999 pt" VSHIFT_QUANTITY="-25.5 pt" BORDER_WIDTH="3.5 px">
+<edge STYLE="sharp_bezier" WIDTH="9"/>
+<font NAME="Inter Medium"/>
+</node>
+<node TEXT="Organizzazione" ID="ID_1914626620" CREATED="1704643315030" MODIFIED="1704644849961" HGAP_QUANTITY="-221.49999 pt" VSHIFT_QUANTITY="47.25 pt" BORDER_WIDTH="3.5 px">
+<edge STYLE="sharp_bezier" WIDTH="9"/>
+<font NAME="Inter Medium"/>
+</node>
+</node>
+</node>
+<node TEXT="Benefici" FOLDED="true" ID="ID_1148311617" CREATED="1704644133931" MODIFIED="1704644839636" HGAP_QUANTITY="-449.49999 pt" VSHIFT_QUANTITY="0 pt" VGAP_QUANTITY="3 pt" BORDER_WIDTH="3.5 px">
+<edge STYLE="sharp_bezier" WIDTH="9"/>
+<font NAME="Inter Medium"/>
+<node TEXT="Efficace" ID="ID_1227681178" CREATED="1704644158093" MODIFIED="1704644818313" HGAP_QUANTITY="-165.99999 pt" VSHIFT_QUANTITY="-18 pt" VGAP_QUANTITY="3 pt" BORDER_WIDTH="3.5 px">
+<edge STYLE="sharp_bezier" WIDTH="9"/>
+<font NAME="Inter Medium"/>
+</node>
+<node TEXT="Stimolante" ID="ID_656018487" CREATED="1704644191973" MODIFIED="1704644820463" HGAP_QUANTITY="-216.99999 pt" VSHIFT_QUANTITY="-2.25 pt" BORDER_WIDTH="3.5 px">
+<edge STYLE="sharp_bezier" WIDTH="9"/>
+<font NAME="Inter Medium"/>
+</node>
+<node TEXT="Intuitivo" ID="ID_1348566476" CREATED="1704644194788" MODIFIED="1704644822346" HGAP_QUANTITY="-192.24999 pt" VSHIFT_QUANTITY="21.75 pt" BORDER_WIDTH="3.5 px">
+<edge STYLE="sharp_bezier" WIDTH="9"/>
+<font NAME="Inter Medium"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Esempi" FOLDED="true" POSITION="bottom_or_right" ID="ID_283407323" CREATED="1704998235695" MODIFIED="1704999152275" HGAP_QUANTITY="142.25 pt" VSHIFT_QUANTITY="-111.75 pt" VGAP_QUANTITY="3 pt" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" UNIFORM_SHAPE="true" TEXT_ALIGN="CENTER">
+<edge STYLE="sharp_bezier" COLOR="#d56231" WIDTH="18"/>
+<font NAME="Inter Medium"/>
+<node TEXT="Elettronica" FOLDED="true" ID="ID_1222603939" CREATED="1704998930261" MODIFIED="1704999233629" HGAP_QUANTITY="73.25 pt" VGAP_QUANTITY="3 pt">
+<edge STYLE="sharp_bezier" WIDTH="12"/>
+<node TEXT="Importanza" FOLDED="true" ID="ID_1608035853" CREATED="1704998933889" MODIFIED="1704999253901" HGAP_QUANTITY="110 pt" VSHIFT_QUANTITY="-18 pt" VGAP_QUANTITY="3 pt">
+<edge STYLE="sharp_bezier" WIDTH="12"/>
+<node TEXT="Identificazione rapida cavi" ID="ID_1090018332" CREATED="1704998974838" MODIFIED="1704999256259" HGAP_QUANTITY="34.25 pt" VSHIFT_QUANTITY="-42 pt">
+<edge STYLE="sharp_bezier" WIDTH="9"/>
+</node>
+<node TEXT="Prevenzione errori" ID="ID_1770198751" CREATED="1704998985500" MODIFIED="1704999257831" HGAP_QUANTITY="62.75 pt" VSHIFT_QUANTITY="-27 pt">
+<edge STYLE="sharp_bezier" WIDTH="9"/>
+</node>
+<node TEXT="Sicurezza ed efficienza" ID="ID_761973017" CREATED="1704998993078" MODIFIED="1704999259512" HGAP_QUANTITY="74.75 pt">
+<edge STYLE="sharp_bezier" WIDTH="9"/>
+</node>
+</node>
+<node TEXT="    Esempi Pratici" FOLDED="true" ID="ID_1658335273" CREATED="1704999023704" MODIFIED="1704999247916" HGAP_QUANTITY="110 pt" VSHIFT_QUANTITY="-21 pt" VGAP_QUANTITY="3 pt">
+<edge STYLE="sharp_bezier" WIDTH="12"/>
+<node TEXT="Diversi colori per diversi tipi di cavi" ID="ID_1996710809" CREATED="1704999025886" MODIFIED="1704999250937" HGAP_QUANTITY="40.25 pt" VSHIFT_QUANTITY="-19.5 pt">
+<edge STYLE="sharp_bezier" WIDTH="9"/>
+</node>
+<node ID="ID_1315289283" CREATED="1704999059228" MODIFIED="1704999247916" HGAP_QUANTITY="32 pt" VSHIFT_QUANTITY="30.75 pt"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <span style="color: rgb(206, 202, 195); font-family: Söhne, ui-sans-serif, system-ui, -apple-system, Segoe UI, Roboto, Ubuntu, Cantarell, Noto Sans, sans-serif, Helvetica Neue, Arial, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji; font-size: 16px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: left; text-indent: 0px; text-transform: none; word-spacing: 0px; white-space: pre-wrap; background-color: rgb(44, 48, 50); display: inline !important; float: none;">Codici colore in schemi elettrici</span>
+  </body>
+</html>
+
+</richcontent>
+<edge STYLE="sharp_bezier" WIDTH="9"/>
+</node>
+</node>
+<node TEXT="Vantaggi" FOLDED="true" ID="ID_746332096" CREATED="1704999082004" MODIFIED="1704999244503" HGAP_QUANTITY="110 pt" VSHIFT_QUANTITY="35.25 pt" VGAP_QUANTITY="3 pt">
+<edge STYLE="sharp_bezier" WIDTH="12"/>
+<node TEXT="Riduzione del rischio di errori" ID="ID_1414491557" CREATED="1704999091626" MODIFIED="1704999244503" HGAP_QUANTITY="47 pt" VSHIFT_QUANTITY="-12 pt">
+<edge STYLE="sharp_bezier" WIDTH="9"/>
+</node>
+<node TEXT="Miglioramento della manutenzione" ID="ID_1341547504" CREATED="1704999104246" MODIFIED="1704999246626" HGAP_QUANTITY="50 pt" VSHIFT_QUANTITY="45 pt">
+<edge STYLE="sharp_bezier" WIDTH="9"/>
+</node>
+</node>
+<node TEXT="Slogan" FOLDED="true" ID="ID_65195644" CREATED="1704999120320" MODIFIED="1704999241452" HGAP_QUANTITY="110 pt" VSHIFT_QUANTITY="66.75 pt" VGAP_QUANTITY="3 pt">
+<edge STYLE="sharp_bezier" WIDTH="12"/>
+<node TEXT="😁💣 &quot;Attenzione alla sicurezza!" ID="ID_531547323" CREATED="1704999128212" MODIFIED="1704999241452" HGAP_QUANTITY="50.75 pt" VSHIFT_QUANTITY="22.5 pt">
+<edge STYLE="sharp_bezier" WIDTH="9"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
